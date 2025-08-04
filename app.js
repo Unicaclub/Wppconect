ccfa280190d439558b65139fd05513ed8fe45d4a
User <unicaclubms.trafego@gmail.com>
2025-08-04 05:36:52 -0300

    Fix app.js code issues and improve syntax

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ wppconnect
     session: 'sessionName',
     catchQR: (base64Qr, asciiQR) => {
       console.log(asciiQR); // Optional to log the QR in the terminal
-      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-        response = {};
+      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+      var response = {};
 
       if (matches.length !== 3) {
         return new Error('Invalid input string');
       }
       response.type = matches[1];
-      response.data = new Buffer.from(matches[2], 'base64');
+      response.data = Buffer.from(matches[2], 'base64');
 
       var imageBuffer = response;
-      require('fs').writeFile(
+      fs.writeFile(
         'out.png',
         imageBuffer['data'],
         'binary',
